55c3092d39aee6abc5e31a6fcbddbeca683a6746
Sam Zhao <zhao.shengjun@gmail.com>
2017-10-09 10:42:54 +1000

    connect redux-devtools in chrome

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 import { createEpicMiddleware } from "redux-observable";
@@ -11,7 +11,12 @@ import { rootEpic } from "./epics";
 
 const epicMiddleware = createEpicMiddleware(rootEpic);
 
-const store = createStore(reducer, applyMiddleware(epicMiddleware));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancer(applyMiddleware(epicMiddleware))
+);
 ReactDOM.render(
   <Provider store={store}>
     <App />
