167b71e92a909050be78cfbcacce00d9bb48799e
Sam Zhao <zhao.shengjun@gmail.com>
2017-10-09 10:21:34 +1000

    use actions to load data

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,16 +1,41 @@
 import React from "react";
 import { connect } from "react-redux";
+import { loadStories, clearStories } from "../actions";
 
 export function Stories(props) {
   return (
-    <pre>
-      <code>{JSON.stringify(props)}</code>
-    </pre>
+    <div>
+      <button type="button" onClick={props.loadStories}>
+        Load Stories
+      </button>
+      <button type="button" onClick={props.clearStories}>
+        Clear Stories
+      </button>
+      <StoryList {...props} />
+    </div>
   );
+}
+
+function StoryList(props) {
+  if (props.items.length === 0) return null;
+  return (
+    <div>{props.items.map(item => <Story {...item} key={item.id} />)}</div>
+  );
+}
+
+function Story(props) {
+  return <p>{props.title}</p>;
 }
 
 function mapState(state) {
   return state;
 }
 
-export default connect(mapState)(Stories);
+function mapDispatch(dispatch) {
+  return {
+    loadStories: () => dispatch(loadStories()),
+    clearStories: () => dispatch(clearStories())
+  };
+}
+
+export default connect(mapState, mapDispatch)(Stories);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,41 @@
+import { LOAD_STORIES, CLEAR_STORIES } from "../actions";
+
+const stories = [
+  {
+    by: "bleakgadfly",
+    id: 14967192,
+    title: "To Protect Voting, Use Open-Source Software",
+    url:
+      "https://mobile.nytimes.com/2017/08/03/opinion/open-source-software-hacker-voting.html"
+  },
+  {
+    by: "mtyurt",
+    id: 14966545,
+    title: "Git: Using Advanced Rebase Features for a Clean Repository",
+    url:
+      "https://mtyurt.net/2017/08/08/git-using-advanced-rebase-feature-for-a-clean-repository.html"
+  },
+  {
+    by: "callumlocke",
+    id: 14967335,
+    title: "Inside an AI Brain: What does Machine Learning Look Like",
+    url: "https://graphcore.ai/posts/what-does-machine-learning-look-like"
+  }
+];
+
 const initialState = {
   items: []
 };
 
 export function storiesReducer(state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case LOAD_STORIES:
+      return { items: stories.slice() };
+    case CLEAR_STORIES:
+      return { items: [] };
+    default:
+      return state;
+  }
 }
 
 export default storiesReducer;
